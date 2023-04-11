6797874b1a9cb7cfa2d0c30a7768520e25551f9a
mishukmahbub <mishukmahbub@gmail.com>
2023-04-12 02:41:10 +0600

    job details page css done

--- ChakriChai-MishukMahbub/src/components/JobDetails.jsx
+++ ChakriChai-MishukMahbub/src/components/JobDetails.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import salary from '../assets/Icons/Frame.png'
+import title from '../assets/Icons/Frame-1.png'
+import phone from '../assets/Icons/Frame-2.png'
+import email from '../assets/Icons/Frame-3.png'
+import address from '../assets/Icons/Frame-4.png'
 
 const JobDetails = () => {
     const id = useParams();
@@ -11,7 +16,7 @@ const JobDetails = () => {
     }, []);
     console.log(details);
     return (
-        <div className='grid grid-cols-3'>
+        <div className='grid grid-cols-3 mx-24 my-32 gap-6'>
             <div className='col-span-2'>
                 <p><span className='font-bold'>Job Description: </span>{details.jobDesc}</p>
                 <br />
@@ -24,8 +29,40 @@ const JobDetails = () => {
                 <br />
             </div>
             <div>
-                <h3 className='font-bold'>Job Details</h3>
+                <div className='bg-blue-100 p-8 rounded-lg'>
+                    <h3 className='font-bold'>Job Details</h3>
+                    <br />
+                    <hr className='border-t-primary' />
+                    <br />
+                    <div className='flex gap-2'>
+                        <img src={salary} className='h-6' alt="" />
+                        <p><span className='font-bold'>Salary:</span> {details.salary} (Per Month)</p>
+                    </div>
+                    <div className='flex gap-2'>
+                        <img src={title} className='h-6' alt="" />
+                        <p><span className='font-bold'>Job Title:</span> {details.position}</p>
+                    </div>
+                    <br />
+                    <h3 className='font-bold'>Contact Information</h3>
+                    <br />
+                    <hr className='border-t-primary' />
+                    <br />
+                    <div className='flex gap-2'>
+                        <img src={phone} className='h-6' alt="" />
+                        <p><span className='font-bold'>Phone:</span> {details.phone}</p>
+                    </div>
+                    <div className='flex gap-2'>
+                        <img src={email} className='h-6' alt="" />
+                        <p><span className='font-bold'>Email:</span> {details.email}</p>
+                    </div>
+                    <div className='flex gap-2'>
+                        <img src={address} className='h-6' alt="" />
+                        <p><span className='font-bold'>Address:</span> {details.address}</p>
+                    </div>
+                    <br />
+                </div>
 
+                <button className='btn btn-primary w-full rounded-lg mt-6'>Apply Now</button>
             </div>
         </div>
     );
