86487328d7d487f5aa657861ef5067ccc294a769
mishukmahbub <mishukmahbub@gmail.com>
2023-04-12 01:23:27 +0600

    job details data loaded

--- ChakriChai-MishukMahbub/src/loaders/jobsCategoryLoader.js
+++ ChakriChai-MishukMahbub/src/loaders/jobsCategoryLoader.js
@@ -1,10 +1,6 @@
 const jobsCategoryLoader = async () => {
     const loadedCategories = await fetch(`jobsCategory.json`);
     const categories = await loadedCategories.json();
-
-    // const loadedJobs = await fetch(`/jobs1234.json`);
-    // const jobs = await loadedJobs.json();
-    // console.log(jobs);
     return categories;
 }
 export default jobsCategoryLoader;
