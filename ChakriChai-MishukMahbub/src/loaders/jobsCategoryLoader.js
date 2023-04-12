9ec727ff2b4167e91d7c21d9c534e2a9a7e32e0e
mishukmahbub <mishukmahbub@gmail.com>
2023-04-12 15:20:43 +0600

    deploy image problem solved

--- ChakriChai-MishukMahbub/src/loaders/jobsCategoryLoader.js
+++
@@ -1,7 +0,0 @@
-const jobsCategoryLoader = async () => {
-    const loadedCategories = await fetch(`jobsCategory.json`);
-    const categories = await loadedCategories.json();
-    
-    return categories;
-}
-export default jobsCategoryLoader;
